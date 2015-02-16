0327a7b6f8831dd828903b8701b81e5336e7b74a
jsanto <jsanto@psych-0574.home>
2015-02-15 19:41:20 -0500

    Initial Committ

--- JJSAssignment 3/ViewController.m
+++ JJSAssignment 3/ViewController.m
@@ -17,11 +17,24 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [webview loadRequest:[NSURLRequest  requestWithURL:[NSURL URLWithString:@"http:www.pitt.edu"]]];
+    
+    
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+-(void)searchBar: (UISearchBar *)searchBar {
+    [searchBar resignFirstResponder];
+    [webview loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"HTTP://%@",searchBar.text]]]];
+   
+}
+
 
 @end
